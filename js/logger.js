32d5f4a2c9acab30f4f68909b7c10d203ef5e029
Fran Espeche <fran.espeche@scale.com>
2025-04-22 14:46:51 -0300

    log log into dd

--- js/logger.js
+++ js/logger.js
@@ -3,12 +3,17 @@ const logger = (message) => {
     info: function (msg) {
       console.info(msg);
     },
+    // this will be sent to datadog
     log: function (msg) {
       console.log(msg);
+      if (window.ritalite?.logger) {
+        window.ritalite.log(JSON.stringify(message), "log");
+      }
     },
+    // this will be sent to datadog
     error: function (msg) {
       console.error(msg);
-      if (window.ritalite.logger) {
+      if (window.ritalite?.logger) {
         window.ritalite.log(JSON.stringify(message), "error");
       }
     },
