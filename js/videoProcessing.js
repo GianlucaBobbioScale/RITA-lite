cc8b5caea22c3d5ed67b934a3e0349bf315497e8
Gianluca Bobbio <gianluca.bobbio@scale.com>
2025-04-23 02:52:25 -0300

    when aborting process, clean the screenshots

--- js/videoProcessing.js
+++ js/videoProcessing.js
@@ -127,6 +127,7 @@ async function processVideo(file, id, pairId, playbackRate, invertedPlaybackRate
       statusLabel.style.color = 'var(--error-color)';
       video.status = 'aborted';
       console.error(`Processing aborted: ${reason}`);
+      video.data.screenshots = [];
       resolve();
       return;
     };
